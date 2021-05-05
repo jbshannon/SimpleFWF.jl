f47442eda82aaa1afd14f0f8e63a430a3e009ba5
Jack Shannon <j.b.shannon@lse.ac.uk>
2021-05-05 23:10:00 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# simpleFWF.jl
-Simple proof-of-concept reader for fixed-width files in Julia
+# `SimpleFWF.jl`
+
+Simple proof-of-concept reader for fixed-width files in Julia. Based on [this discussion](https://discourse.julialang.org/t/reading-fixed-width-files-a-preliminary-solution/60525) on the JuliaLang Discourse.
+
+## Examples
+
+```julia-repl
+julia> ranges = (10:43, 53:60)
+
+julia> df = readfwf("sample.dat", ranges)
+```
---
+++ main.jl
@@ -0,0 +1,54 @@
+module SimpleFWF
+
+using DataFrames, CSV
+
+export readfwf, readfwf!
+
+function fwfline(line, ranges; stripstr=true)
+    data = String[]
+    for r in ranges
+        ldata = stripstr ? strip(SubString(line, r)) : SubString(line, r)
+        push!(data, ldata)
+    end
+    data
+end
+
+"""
+    readfwf(source, ranges; stripstr=true) -> DataFrame
+
+Reads a fixed-width file `source` into a `DataFrame` using pre-defined column widths in `ranges`.
+
+# Examples
+```julia-repl
+julia> df = readfwf("sample.dat", (10:43, 53:60))
+```
+"""
+function readfwf(source, ranges; stripstr=true)
+    lines = eachline(source)
+
+    # Construct DataFrame
+    firstline, lines = Base.Iterators.peel(lines)
+    colnames = fwfline(firstline, ranges)
+    columns = Dict(Symbol(c) => String[] for c in colnames)
+    df = DataFrame(columns)
+
+    # Read data
+    for line in lines
+        push!(df, fwfline(line, ranges; stripstr))
+    end
+    df
+end
+
+function readfwf!(df, source, ranges; stripstr=true)
+    lines = eachline(source)
+
+    # Ignore first line
+    firstline, lines = Base.Iterators.peel(lines)
+
+    # Read data
+    for line in lines
+        push!(df, fwfline(line, ranges; stripstr))
+    end
+end
+
+end
